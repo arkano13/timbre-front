ebabb80f227ceff5d30f61939f9016b396f4bd79
Gelsyn <gelsynfigu@gmail.com>
2026-04-16 13:20:02 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use(express.static(__dirname))
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'TIMBRE-Escolar.html'))
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Frontend corriendo en puerto ${PORT}`))
